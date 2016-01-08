bf7fbe03ed3450066b7300f666d0c43bf15b4455
moose0030 <matthewpreston@hotmail.ca>
2016-01-07 20:30:27 -0500

    First commit

---
+++ app/src/main/java/com/example/matthewpreston/bluetoothtransit/BluetoothClient.java
@@ -0,0 +1,11 @@
+package com.example.matthewpreston.bluetoothtransit;
+
+/**
+ * Created by matthewpreston on 2016-01-07.
+ */
+public class BluetoothClient {
+
+    public BluetoothClient(){
+
+    }
+}
